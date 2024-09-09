da08820d4a1dfb787bfc57d6cb9ad9626414b885
Brianna Becker <bbecker@seatgeek.com>
2024-09-09 21:47:42 +0200

    Remove unneeded imports

--- wasm/bridge_wasm_exec.js
+++ wasm/bridge_wasm_exec.js
@@ -1,11 +1,6 @@
 "use strict";
 
 globalThis.require = require;
-globalThis.fs = require("fs");
-globalThis.TextEncoder = require("util").TextEncoder;
-globalThis.TextDecoder = require("util").TextDecoder;
-
-globalThis.performance ??= require("performance");
 
 if (!globalThis.crypto) {
 	const crypto = require("crypto");
